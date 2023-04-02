cb3548ac0bd0602f2f4a8bb1c356c1963c03c1c4
Michael Christman <mchristman@my365.bellevue.edu>
2023-04-02 13:43:14 -0500

    Create index.js

---
+++ web-330/week-3/scripts/index.js
@@ -0,0 +1,12 @@
+/** 
+   Title: index.js
+   Author: Michael Christman
+   Date: March 31st, 2023
+   Description: Module index.js
+*/
+export * from "scripts/appetizer.js";
+export * from "scripts/beverage.js";
+export * from "scripts/dessert.js";
+export * from "scripts/main-course.js";
+export * from "scripts/bill.js";
+export * from "scripts/product.js";
