741d5a38ad47ce8f4c4afd982d5dc408a2681c9f
Michael Christman <mchristman@my365.bellevue.edu>
2023-04-02 13:48:40 -0500

    Create product.js

---
+++ web-330/week-3/scripts/product.js
@@ -0,0 +1,18 @@
+/** 
+   Title: product.js
+   Author: Michael Christman
+   Date: March 31st, 2023
+   Description: Module product.js
+*/
+
+export class Product {
+
+    constructor (home, price) {
+
+       this.name = name;
+       this.price = price;
+       
+    }
+
+
+ }
