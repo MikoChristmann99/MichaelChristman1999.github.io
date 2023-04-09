cedf098ff4cfb8850b3ce6a79b5c622bc9d2d675
Michael Christman <mchristman@my365.bellevue.edu>
2023-04-09 13:52:53 -0500

    Create food-model.js

---
+++ web-330/week-4/food-model.js
@@ -0,0 +1,16 @@
+/*
+    ==================
+    Title: food-model.js, 
+    Author: Michael Christman
+    Date: April 7th, 2023
+    Description: JavaScript code file where I created the food model class
+*/
+
+export class FoodModel {
+    constructor(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+}
+
