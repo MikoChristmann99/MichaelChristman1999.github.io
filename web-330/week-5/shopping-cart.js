4af03be8be99cdaac167c8289857bee6e29e491f
Michael Christman <mchristman@my365.bellevue.edu>
2023-04-13 23:40:09 -0500

    Update shopping-cart.js

--- web-330/week-5/shopping-cart.js
+++ web-330/week-5/shopping-cart.js
@@ -2,20 +2,20 @@
 /*
     ==================
     Title: shopping-cart.js, 
-    Author: William Austin
-    Date: 4/12/2023
-    Description: Creating a ShoppingCart Function Module for Repair Shop App
+    Author: Michael Christman
+    Date: March 14th, 2023
+    Description: In this JavaScript file, I created a ShoppingCart function module for the Repair Shop app assignment.
 */
 
 export class ShoppingCart {
     constructor(){
-        this.products=[]; //the cart is an empty array of products
+        this.products=[]; //the shopping cart is simply an empty array of products
     }
     count(){
-        return this.products.length; //the count in how many products are in the cart
+        return this.products.length; //count exactly how many products are in the cart
     }
     add(product){
-        this.products.push(product); //the add function, adds products to the cart
+        this.products.push(product); //addition function, pretty self-explanatory: adds more products to the cart
     }
     *[Symbol.iterator](){
         for(let x of this.products){
