6460fd5d8b9ef295cc10dd38d7f0ccb3bf11c9ac
Michael Christman <mchristman@my365.bellevue.edu>
2023-04-13 23:19:31 -0500

    Create shopping-cart.js

---
+++ web-330/week-5/shopping-cart.js
@@ -0,0 +1,26 @@
+
+/*
+    ==================
+    Title: shopping-cart.js, 
+    Author: William Austin
+    Date: 4/12/2023
+    Description: Creating a ShoppingCart Function Module for Repair Shop App
+*/
+
+export class ShoppingCart {
+    constructor(){
+        this.products=[]; //the cart is an empty array of products
+    }
+    count(){
+        return this.products.length; //the count in how many products are in the cart
+    }
+    add(product){
+        this.products.push(product); //the add function, adds products to the cart
+    }
+    *[Symbol.iterator](){
+        for(let x of this.products){
+            yield x;
+        }
+
+    }
+}
